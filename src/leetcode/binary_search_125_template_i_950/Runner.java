21fca46fcd97c79506068148b033182b57c43f15
Đinh Hữu Hiếu <50437934+DinhHuuHieu1999VN@users.noreply.github.com>
2022-12-09 16:33:27 +0700

    update leetcode 09/12/2022_16:28

---
+++ src/leetcode/binary_search_125_template_i_950/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.binary_search_125_template_i_950;
+
+public class Runner {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.mySqrt(4));
+    }
+}
--- src/leetcode/binary_search_138_background_1038/Main.java
+++ src/leetcode/binary_search_138_background_1038/Main.java
@@ -1,27 +1,59 @@
 package leetcode.binary_search_138_background_1038;
 
-import java.util.HashMap;
-
 public class Main {
-    public int search(int[] nums, int target) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        int k = 0;
-        for (int i = 0; i <= nums.length - 1; i++) {
-            if (hashMap.containsValue(nums[i]) == false) {
-                hashMap.put(i, nums[i]);
-            }
+    //    public int search(int[] nums, int target) {
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            arrayList.add(nums[i]);
+//        }
+//        return arrayList.indexOf(target);
+//    }
 
-        }
-        for (int j = 0; j <= hashMap.size() - 1; j++) {
-            if (hashMap.containsValue(target) == true) {
-                if (hashMap.get(j) == target) {
-                    k = j;
-                }
+//    public int search(int[] nums, int target) {
+//        TreeSet<Integer> treeSet = new TreeSet<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            treeSet.add(nums[i]);
+//        }
+//        return indexOf(treeSet, target);
+//    }
+//
+//    private static int indexOf(TreeSet<Integer> treeSet, Integer element) {
+//        List<Integer> list = new ArrayList<>(treeSet);
+//        return list.indexOf(element);
+//    }
+
+//    public int search(int[] nums, int target) {
+//        int left = 0, right = nums.length - 1;
+//        while (left <= right) {
+//            int m = left + (right - left) / 2;
+//            if (nums[m] == target)
+//                return m;
+//            if (nums[m] < target)
+//                left = m + 1;
+//            else
+//                right = m - 1;
+//        }
+//        return -1;
+//    }
+
+    int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
+
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            // Prevent (left + right) overflow
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                k = -1;
+                right = mid - 1;
             }
         }
 
-        return k;
+        // End Condition: left > right
+        return -1;
     }
 }
